898c47aac18f18952682f9a53557bcfdb55e61ca
Ahmed Amshir <ahmd.amshir@gmail.com>
2025-11-03 17:50:20 +0200

    G

--- resources/views/orders/details-layout.blade.php
+++ resources/views/orders/details-layout.blade.php
@@ -21,7 +21,7 @@
 
     <!-- Footer -->
     <footer class="bg-dark text-white text-center py-3">
-        <p>&copy; {{ date('Y') }} Your Company. All rights reserved.</p>
+        <p>&copy; {{ date('Y') }} Soul. All rights reserved.</p>
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
