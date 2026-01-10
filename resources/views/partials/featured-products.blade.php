71f57e11b28197d803e4dbc10669c66c2013bd30
Ahmed Amshir <ahmedamshir@Ahmeds-MacBook-Pro.local>
2026-01-10 11:24:25 +0200

    Fix

--- resources/views/partials/featured-products.blade.php
+++ resources/views/partials/featured-products.blade.php
@@ -48,7 +48,7 @@
                             </div>
                             <div class="product-info">
                                 <h3>{{ $variation->name }}</h3>
-                                <p>{!! $variation->description !!}</p>
+                                <p>{{ Str::limit(strip_tags($variation->description), 100, ' ...') }}</p>
                                 <div class="product-price">{{ number_format($variation->price, 0) }} LE</div>
                             </div>
                         </a>
